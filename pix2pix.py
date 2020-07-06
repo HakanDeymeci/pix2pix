ccca338cfa87d51a0d072b11b210c60619a01ab1
Krikkits <64783148+Krikkits@users.noreply.github.com>
2020-07-06 16:53:54 +0200

    function to load dataset

--- pix2pix.py
+++ pix2pix.py
@@ -1,6 +1,13 @@
 #libraries
 
 #Import Dataset
+DATA_URL = 'https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/cityscapes.tar.gz'
+
+path_to_zip = tf.keras.utils.get_file('cityscapes.tar.gz',
+                                      origin=DATA_URL,
+                                      extract=True)
+
+PATH = os.path.join(os.path.dirname(path_to_zip), 'cityscapes/')
 
 #Randomizing
 
