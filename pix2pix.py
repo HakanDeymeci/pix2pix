65990ec22c353a8c6c695ca8069aecb046660adc
Krikkits <64783148+Krikkits@users.noreply.github.com>
2020-07-07 14:43:27 +0200

    Added generator loss

--- pix2pix.py
+++ pix2pix.py
@@ -154,7 +154,16 @@ def get_up_stack():
 generator = Generator()
 
 #Generator loss
+LAMBDA = 100 #Lambda according to paper
+def generator_loss(disc_generated_output, gen_output, target):
+  gan_loss = loss_object(tf.ones_like(disc_generated_output), disc_generated_output)
 
+  # mean absolute error between the generated image and the target image
+  l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
+
+  total_gen_loss = gan_loss + (LAMBDA * l1_loss)
+
+  return total_gen_loss, gan_loss, l1_loss
 
 
 #Discriminator
