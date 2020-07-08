584b3293e37addb5c3a2a001226cf0c14f595742
HakanDeymeci <64503346+HakanDeymeci@users.noreply.github.com>
2020-07-08 10:20:41 +0200

    Update pix2pix.py
    Added Discriminator, Discriminator Loss and Optimizers for Generator and Discriminator

--- pix2pix.py
+++ pix2pix.py
@@ -187,11 +187,61 @@ def generator_loss(disc_generated_output, gen_output, target):
 
 
 #Discriminator
+def Discriminator():
+
+  inp = tf.keras.layers.Input(shape=[256, 256, 3], name='input_image')
+  tar = tf.keras.layers.Input(shape=[256, 256, 3], name='target_image')
+
+  x = tf.keras.layers.concatenate([inp, tar]) # (bs, 256, 256, channels*2)
+
+
+  e = 0
+  d= 64
+  while e in range(0,3):
+    if e==0:
+      down = downsample(d, 4, False)(x)
+      d=d*2
+    else:down = downsample(d, 4)(down)
+    d=d*2
+    e += 1
+
+
+  zero_pad1 = tf.keras.layers.ZeroPadding2D()(down) # (bs, 34, 34, 256)
+  conv = tf.keras.layers.Conv2D(512, 4, strides=1,
+                                kernel_initializer=tf.random_normal_initializer(0., 0.03),
+                                use_bias=False)(zero_pad1) # (bs, 31, 31, 512)
+
+  batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
+
+  leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
+
+  zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu) # (bs, 33, 33, 512)
+
+  last = tf.keras.layers.Conv2D(1, 4, strides=1,
+                                kernel_initializer=tf.random_normal_initializer(0., 0.03))(zero_pad2) # (bs, 30, 30, 1)
+                                
+  return tf.keras.Model(inputs=[inp, tar], outputs=last)
+
+discriminator = Discriminator()
 
 #Discriminator loss
 
+loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def discriminator_loss(disc_real_output, disc_generated_output):
+  real_loss = loss_object(tf.ones_like(disc_real_output), disc_real_output)
+
+  generated_loss = loss_object(tf.zeros_like(disc_generated_output), disc_generated_output)
+
+  total_disc_loss = real_loss + generated_loss
+
+  return total_disc_loss
+
 #Optimizers
 
+generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
+discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 
 #Generating images
 def generate_images(model, test_input, tar):
